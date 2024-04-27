b26578cab21f093a43ab298106e30b190b91225d
loklok2 <loklok2@DESKTOP-67D02MR>
2024-04-27 18:04:58 +0900

    20240427 pnutest_java_complete

--- PnuTest/com/ruby/java/ch07/abstraction/ch07_test_인터페이스작성연습.java
+++ PnuTest/com/ruby/java/ch07/abstraction/ch07_test_인터페이스작성연습.java
@@ -3,14 +3,14 @@ package com.ruby.java.ch07.abstraction;
  * 7장 실습 코드로 구현한다 
  */
 
-interface Shape {
-	public double perimeter();
-	public float area();
+interface Shape {   // 인터페이스 Shape는 perimeter()와 area()메소드를 정의
+	public double perimeter(); //둘레 반환
+	public float area(); //면적 반환
 }
 
-class Triangle implements Shape {
-	private int x1,y1,x2,y2,x3,y3;
-	
+class Triangle implements Shape { // Triangle 클래스는 shape 인터페이스를 구현중
+	private int x1,y1,x2,y2,x3,y3; 
+	//세점의 입력값을 받아 초기화
 	public Triangle(int x1, int y1, int x2, int y2, int x3, int y3) {
 		this.x1 = x1;
 		this.y1 = y1;
@@ -34,12 +34,13 @@ class Triangle implements Shape {
 	    float s = (ss1 + ss2 + ss3) / 2.0f;
 	    float area = (float) Math.sqrt(s * (s - ss1) * (s - ss2) * (s - ss3));
 	    return area;
-	}    
+	}
 	    //float은 4바이트, double은 8바이트 숫자의 범위차이 때문에 double이 더 정확할 거 같음
 
 }
-class Rectangle implements Shape {
+class Rectangle implements Shape { //Rectangle 클래스는 Shape 인터페이스를 구현
 	int x1,y1,x2,y2;
+	// 두점의 좌표값을 받아 초기화
 	public Rectangle(int x1, int y1, int x2, int y2) {
 		this.x1 = x1;
 		this.y1 = y1;
@@ -84,6 +85,7 @@ class Rectangle implements Shape {
 class Square extends Rectangle { // Square는 Rextangle의 클래스를 상속받음
 //	int x1, y1;
 //	int side;
+	// Square는 정사격형이니까, Ractangle의 클래스를 상속받아 길이와 폭이 같음
 	public Square (int x1, int y1, int side){
 		super(x1, y1, x1 + side, y1 + side);
 	}
@@ -98,6 +100,8 @@ public class ch07_test_인터페이스작성연습 {
 	System.out.println("삼각형 면적 = " + t.area());
 	System.out.println("사각형 둘레 길이 = " + r.perimeter());
 	System.out.println("사각형 면적 = " + r.area());
+	System.out.println("정사각형 둘레 길이 = " + s.perimeter());
+	System.out.println("정사각형 면적 = " + s.area());
 	}
 }
 
